3257a437607f21b37b4fc7ce88f7b9444763cde5
n-Turn <nicturn@vt.edu>
2024-03-15 14:59:16 -0400

    better code formatting

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 // Navigation Bar
-const menu = document.querySelector('#mobile-menu');
-const menuLinks = document.querySelector('.navbar__menu');
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".navbar__menu");
 
-menu.addEventListener('click', function()
-{
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active');
-
+menu.addEventListener("click", function () {
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("active");
 });
 
 // Image Slider
@@ -17,44 +15,35 @@ let slideIndex = 0;
 
 document.addEventListener("DOMContentLoaded", initializeSlider);
 
-function initializeSlider()
-{
-    if(slides.length > 0)
-    {
-        slides[slideIndex].classList.add("displaySlide");
-        // intervalId = setInterval(nextSlide, 10000);
-    }
-   
-}
-
-function showSlide(index)
-{
-    if (index >= slides.length)
-    {
-        slideIndex = 0;
-    }
-    else if (index < 0)
-    {
-        slideIndex = slides.length - 1;
-    }
-    slides.forEach(slide => {slide.classList.remove("displaySlide")});
+function initializeSlider() {
+  if (slides.length > 0) {
     slides[slideIndex].classList.add("displaySlide");
-    
+    // intervalId = setInterval(nextSlide, 10000);
+  }
 }
 
-function prevSlide()
-{
-    slideIndex--;
-    showSlide(slideIndex);
-    //clearInterval(intervalId);
+function showSlide(index) {
+  if (index >= slides.length) {
+    slideIndex = 0;
+  } else if (index < 0) {
+    slideIndex = slides.length - 1;
+  }
+  slides.forEach((slide) => {
+    slide.classList.remove("displaySlide");
+  });
+  slides[slideIndex].classList.add("displaySlide");
 }
 
-function nextSlide()
-{
-    slideIndex++;
-    showSlide(slideIndex);
+function prevSlide() {
+  slideIndex--;
+  showSlide(slideIndex);
+  //clearInterval(intervalId);
 }
 
+function nextSlide() {
+  slideIndex++;
+  showSlide(slideIndex);
+}
 
 // Slide caraousel
 
@@ -63,65 +52,75 @@ const carousel = document.querySelector(".carousel");
 const firstCardWidth = carousel.querySelector(".card").offsetWidth;
 const arrowBtns = document.querySelectorAll(".wrapper i");
 const carouselChildrens = [...carousel.children];
-let isDragging = false, isAutoPlay = true, startX, startScrollLeft, timeoutId;
+let isDragging = false,
+  isAutoPlay = true,
+  startX,
+  startScrollLeft,
+  timeoutId;
 // Get the number of cards that can fit in the carousel at once
 let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
 // Insert copies of the last few cards to beginning of carousel for infinite scrolling
-carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
+carouselChildrens
+  .slice(-cardPerView)
+  .reverse()
+  .forEach((card) => {
     carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
-});
+  });
 // Insert copies of the first few cards to end of carousel for infinite scrolling
-carouselChildrens.slice(0, cardPerView).forEach(card => {
-    carousel.insertAdjacentHTML("beforeend", card.outerHTML);
+carouselChildrens.slice(0, cardPerView).forEach((card) => {
+  carousel.insertAdjacentHTML("beforeend", card.outerHTML);
 });
 // Scroll the carousel at appropriate postition to hide first few duplicate cards on Firefox
 carousel.classList.add("no-transition");
 carousel.scrollLeft = carousel.offsetWidth;
 carousel.classList.remove("no-transition");
 // Add event listeners for the arrow buttons to scroll the carousel left and right
-arrowBtns.forEach(btn => {
-    btn.addEventListener("click", () => {
-        carousel.scrollLeft += btn.id == "left" ? -firstCardWidth : firstCardWidth;
-    });
+arrowBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    carousel.scrollLeft += btn.id == "left" ? -firstCardWidth : firstCardWidth;
+  });
 });
 const dragStart = (e) => {
-    isDragging = true;
-    carousel.classList.add("dragging");
-    // Records the initial cursor and scroll position of the carousel
-    startX = e.pageX;
-    startScrollLeft = carousel.scrollLeft;
-}
+  isDragging = true;
+  carousel.classList.add("dragging");
+  // Records the initial cursor and scroll position of the carousel
+  startX = e.pageX;
+  startScrollLeft = carousel.scrollLeft;
+};
 const dragging = (e) => {
-    if(!isDragging) return; // if isDragging is false return from here
-    // Updates the scroll position of the carousel based on the cursor movement
-    carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
-}
+  if (!isDragging) return; // if isDragging is false return from here
+  // Updates the scroll position of the carousel based on the cursor movement
+  carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+};
 const dragStop = () => {
-    isDragging = false;
-    carousel.classList.remove("dragging");
-}
+  isDragging = false;
+  carousel.classList.remove("dragging");
+};
 const infiniteScroll = () => {
-    // If the carousel is at the beginning, scroll to the end
-    if(carousel.scrollLeft === 0) {
-        carousel.classList.add("no-transition");
-        carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth);
-        carousel.classList.remove("no-transition");
-    }
-    // If the carousel is at the end, scroll to the beginning
-    else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth) {
-        carousel.classList.add("no-transition");
-        carousel.scrollLeft = carousel.offsetWidth;
-        carousel.classList.remove("no-transition");
-    }
-    // Clear existing timeout & start autoplay if mouse is not hovering over carousel
-    clearTimeout(timeoutId);
-    if(!wrapper.matches(":hover")) autoPlay();
-}
+  // If the carousel is at the beginning, scroll to the end
+  if (carousel.scrollLeft === 0) {
+    carousel.classList.add("no-transition");
+    carousel.scrollLeft = carousel.scrollWidth - 2 * carousel.offsetWidth;
+    carousel.classList.remove("no-transition");
+  }
+  // If the carousel is at the end, scroll to the beginning
+  else if (
+    Math.ceil(carousel.scrollLeft) ===
+    carousel.scrollWidth - carousel.offsetWidth
+  ) {
+    carousel.classList.add("no-transition");
+    carousel.scrollLeft = carousel.offsetWidth;
+    carousel.classList.remove("no-transition");
+  }
+  // Clear existing timeout & start autoplay if mouse is not hovering over carousel
+  clearTimeout(timeoutId);
+  if (!wrapper.matches(":hover")) autoPlay();
+};
 const autoPlay = () => {
-    if(window.innerWidth < 800 || !isAutoPlay) return; // Return if window is smaller than 800 or isAutoPlay is false
-    // Autoplay the carousel after every 2500 ms
-    timeoutId = setTimeout(() => carousel.scrollLeft += firstCardWidth, 2500);
-}
+  if (window.innerWidth < 800 || !isAutoPlay) return; // Return if window is smaller than 800 or isAutoPlay is false
+  // Autoplay the carousel after every 2500 ms
+  timeoutId = setTimeout(() => (carousel.scrollLeft += firstCardWidth), 2500);
+};
 autoPlay();
 carousel.addEventListener("mousedown", dragStart);
 carousel.addEventListener("mousemove", dragging);
@@ -131,27 +130,29 @@ wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
 wrapper.addEventListener("mouseleave", autoPlay);
 
 // Fade
-document.addEventListener('DOMContentLoaded', () => {
-    const sections = document.querySelectorAll('.fade');
-  
-    const observerOptions = {
-      threshold: 0.1, // Trigger the animation when at least 10% of the section is visible
-    };
-  
-    const observerCallback = (entries, observer) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add('inview2');
-        } else {
-          entry.target.classList.remove('inview2');
-        }
-      });
-    };
-  
-    const sectionObserver = new IntersectionObserver(observerCallback, observerOptions);
-  
-    sections.forEach((section) => {
-      sectionObserver.observe(section);
+document.addEventListener("DOMContentLoaded", () => {
+  const sections = document.querySelectorAll(".fade");
+
+  const observerOptions = {
+    threshold: 0.1, // Trigger the animation when at least 10% of the section is visible
+  };
+
+  const observerCallback = (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("inview2");
+      } else {
+        entry.target.classList.remove("inview2");
+      }
     });
+  };
+
+  const sectionObserver = new IntersectionObserver(
+    observerCallback,
+    observerOptions
+  );
+
+  sections.forEach((section) => {
+    sectionObserver.observe(section);
   });
-  
+});
